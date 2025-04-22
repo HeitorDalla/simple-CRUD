59043407eb5f2c8b76d5f4307c6470097507f27c
HeitorDalla <heitorvillavilla@gmail.com>
2025-04-22 19:31:54 -0300

    A logica do formulario iniciada, chamando o listenner de submit

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 "use strict";
 
+const form = document.querySelector("#form");
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    event.preventDefault();
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+    
+        
+    });
+});
