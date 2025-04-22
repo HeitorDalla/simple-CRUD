997d267bf7f6094a681ca4c6137d337985e68f6a
HeitorDalla <heitorvillavilla@gmail.com>
2025-04-22 20:15:24 -0300

    Adicionado todo o conteudo dentro da main principal e criado a div das tarefas cadastradas, com o objeto de tornar o projeto mais semântico

--- script.js
+++ script.js
@@ -19,6 +19,8 @@ document.addEventListener("DOMContentLoaded", (event) => {
     });
 
     limpar.addEventListener("click", (event) => {
+        event.preventDefault();
+        
         limparValores();
     });
 });
